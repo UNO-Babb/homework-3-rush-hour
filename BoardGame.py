a757aa60eb631383441d70412b4c20723f7866c8
aatikpohou <aatikpohou@unomaha.edu>
2025-04-11 15:48:42 +0000

    almost

--- BoardGame.py
+++ BoardGame.py
@@ -1,23 +1,41 @@
-#Example Flask App for a hexaganal tile game
-#Logic is in this python file
 
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-# Keep track of the current player
-current_player = 1
+# Initialize board for checkers
+
+def initial_board():
+    board = [['' for _ in range(8)] for _ in range(8)]
+    for row in range(3):
+        for col in range(8):
+            if (row + col) % 2 == 1:
+                board[row][col] = 'P2'
+    for row in range(5, 8):
+        for col in range(8):
+            if (row + col) % 2 == 1:
+                board[row][col] = 'P1'
+    return board
+
+board_state = initial_board()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', board=board_state)
 
-@app.route('/add_tile')
-def add_tile():
-    global current_player
-    color = 'green' if current_player == 1 else 'blue'  # Alternate between green and blue
-    current_player = 2 if current_player == 1 else 1  # Switch to the other player
-    return jsonify(color=color)
+@app.route('/move', methods=['POST'])
+def move():
+    data = request.json
+    from_pos = data['from']
+    to_pos = data['to']
+    fr = 8 - int(from_pos[1])
+    fc = ord(from_pos[0]) - ord('a')
+    tr = 8 - int(to_pos[1])
+    tc = ord(to_pos[0]) - ord('a')
+    piece = board_state[fr][fc]
+    board_state[fr][fc] = ''
+    board_state[tr][tc] = piece
+    return jsonify(success=True)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
